c8fefb15f5eeaceedf7fe44bf4f96d0ea0c27fc1
soyyogi <raoyogesh78@yahoo.in>
2021-02-04 01:11:26 +0100

    #6 added new item drop down

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
-  session_start();
-  // unset($_SESSION['currentPath']);
-  $root = 'root';
-  if(!(file_exists($root) && is_dir($root))) {
-    mkdir($root);
-  }
-  if (!isset($_SESSION['currentPath'])) {
-    $_SESSION['currentPath'] = $root;
-  }
-  $currentTree = array_slice(scandir($_SESSION['currentPath']), 2);
-  // print_r($currentTree);
+session_start();
+// unset($_SESSION['currentPath']);
+$root = 'root';
+if (!(file_exists($root) && is_dir($root))) {
+  mkdir($root);
+}
+if (!isset($_SESSION['currentPath'])) {
+  $_SESSION['currentPath'] = $root;
+}
+$currentTree = array_slice(scandir($_SESSION['currentPath']), 2);
+// print_r($currentTree);
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,6 +24,7 @@
   <link rel="stylesheet" href="./index.css">
   <title>File Sistem</title>
 </head>
+
 <body>
   <header>
     <div class="header">
@@ -30,7 +32,14 @@
       <input type="search" class="input-search" placeholder="search">
       <ul class="header-list">
         <li><a href="#" class="header-link">Upload</a></li>
-        <li><a href="#" class="header-link">New Item</a></li>
+        <li class="create-new-item">
+          <a href="#" class="header-link">New Item</a>
+          <ul class="item-options">
+            <li>Text File</li>
+            <li>MS Word File</li>
+            <li>Folder</li>
+          </ul>
+        </li>
       </ul>
     </div>
   </header>
@@ -38,13 +47,14 @@
     <h2>This will be the main body part</h2>
     <h3><a href="http://localhost/PHP_FileSystem_explorer/actions.php?action=previousDir">&#8617;</a></h3>
     <ul>
-<?php
-  foreach( $currentTree as $i => $name) {
-    echo '<li><a href="http://localhost/PHP_FileSystem_explorer/actions.php?name='.$name.'&action=open">'. $name . '</a></li>';
-  }
+      <?php
+      foreach ($currentTree as $i => $name) {
+        echo '<li><a href="http://localhost/PHP_FileSystem_explorer/actions.php?name=' . $name . '&action=open">' . $name . '</a></li>';
+      }
 
-?>
+      ?>
     </ul>
   </main>
 </body>
+
 </html>
