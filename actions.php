8275b74b68fc243a2fc7a7c636c79e4b5c926150
soyyogi <raoyogesh78@yahoo.in>
2021-02-04 03:09:34 +0100

    #6 create new .txt file and refactor create new folder

--- actions.php
+++ actions.php
@@ -3,13 +3,19 @@ session_start();
 
 if($_POST['type']) {
     $type = $_POST['type'];
+    $path = $_SESSION['currentPath'] . '/' . $_POST['title'];
     switch ($type) {
         case 'dir':
-            mkdir($_SESSION['currentPath'] . '/' . $_POST['title']);
-            header('Location: http://localhost/PHP_FileSystem_explorer');
+            if(!(file_exists($path) && is_dir($path))) {
+                mkdir($path);
+            }
             break;
         case 'txt':
-            
+            if(!(file_exists($path . '.txt') && is_file($path . '.txt'))) {
+                $file = fopen($path . '.txt', 'a+');
+                fwrite($file, $_POST['body']);
+                fclose($file);
+            }
             break;
         case 'docx':
 
@@ -17,6 +23,7 @@ if($_POST['type']) {
         default:
             echo 'unsupported type';
     }
+    header('Location: http://localhost/PHP_FileSystem_explorer');
 }
 
 if(isset($_GET['action'])){
